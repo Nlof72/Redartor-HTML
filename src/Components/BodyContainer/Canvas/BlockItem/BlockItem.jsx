c690401ed31831919510f7f9f42cb37d91d2631a
Anastasia Abazovskaya <ryabayeah@gmail.com>
2021-12-22 01:13:51 +0700

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/Components/BodyContainer/Canvas/BlockItem/BlockItem.jsx
+++ src/Components/BodyContainer/Canvas/BlockItem/BlockItem.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useRef} from "react";
+import React, {useCallback, useRef, useState} from "react";
 import {styled} from "@mui/material/styles";
 import {Box, IconButton} from "@mui/material";
 import ClearIcon from '@mui/icons-material/Clear';
@@ -25,33 +25,62 @@ const BlockItem = (props) => {
     const currentBlock = useSelector((state) => state?.canvasData?.currentBlock);
     const currentCanvas = useSelector((state) => state?.canvasData?.canvas)
     const dispatch = useDispatch();
+    const [children, setChildren] = useState([]);
 
     const moveItem = useCallback((dragIndex, dragId, hoverIndex) => {
+
             let newCanvas = JSON.parse(JSON.stringify(currentCanvas));
             let draggedItem = newCanvas.map(elem => elem.find(ele => ele.id === dragId)).find(el => el !== undefined);
             let currentBlockItems = newCanvas[draggedItem.blockIndex];
+            // let children = currentBlockItems.filter(item => item.parentId === draggedItem.id);
+            // if (children.length !== 0) {
+            //
+            // }
 
             [currentBlockItems[dragIndex], currentBlockItems[hoverIndex]] = [currentBlockItems[hoverIndex], currentBlockItems[dragIndex]];
+            // let leftSide = currentBlockItems.slice(0, hoverIndex);
+            // let rightSide =  currentBlockItems.slice(hoverIndex);
             dispatch(UpdateCanvas(JSON.parse(JSON.stringify(newCanvas))))
         },
         [currentCanvas]);
-
     const moveItemToBlock = useCallback((draggedItemId, dropBlockIndex) => {
+
             let newCanvas = JSON.parse(JSON.stringify(currentCanvas));
             let draggedItem = newCanvas.map(elem => elem.find(ele => ele.id === draggedItemId)).find(el => el !== undefined);
-            if (draggedItem.blockIndex === dropBlockIndex) {
+            if (draggedItem.blockIndex === dropBlockIndex && draggedItem.parentId === 0) {
                 return;
             }
-            newCanvas[draggedItem.blockIndex] = newCanvas[draggedItem.blockIndex].filter(item=>item.id !== draggedItemId);
+            newCanvas[draggedItem.blockIndex] = newCanvas[draggedItem.blockIndex].filter(item => item.id !== draggedItemId);
             draggedItem.blockIndex = dropBlockIndex;
+            draggedItem.parentId = 0;
             newCanvas[dropBlockIndex].push(draggedItem);
+            console.log(newCanvas, "***newCanvas****1****")
             dispatch(UpdateCanvas(newCanvas))
+        },
+        [currentCanvas]);
+
+    const moveItemIntoDiv = useCallback((dragIndex, dragId, hoverIndex) => {
+
+            let newCanvas = JSON.parse(JSON.stringify(currentCanvas));
+            let draggedItem = newCanvas.map(elem => elem.find(ele => ele.id === dragId)).find(el => el !== undefined);
+
+            if (newCanvas[draggedItem.blockIndex][hoverIndex].componentType !== "div") return;
+            if (draggedItem.parentId === newCanvas[draggedItem.blockIndex][hoverIndex].id) return;
+            if (dragIndex + 1 === hoverIndex) return;
+
+            draggedItem.parentId = newCanvas[draggedItem.blockIndex][hoverIndex].id;
+            newCanvas[draggedItem.blockIndex] = newCanvas[draggedItem.blockIndex].filter((item, index) => index !== dragIndex);
+
+            let leftPart = newCanvas[draggedItem.blockIndex].slice(0, hoverIndex);
+            let rightPart = newCanvas[draggedItem.blockIndex].slice(hoverIndex);
+            newCanvas[draggedItem.blockIndex] = [...leftPart, draggedItem, ...rightPart]
+            console.log(newCanvas, "*******newCanvas*****")
+            dispatch(UpdateCanvas(JSON.parse(JSON.stringify(newCanvas))))
         },
         [currentCanvas]);
 
 
     const ref = useRef(null);
-
     const [{handlerId}, drop] = useDrop({
         accept: ItemTypes.CARD,
         collect(monitor) {
@@ -60,8 +89,10 @@ const BlockItem = (props) => {
             };
         },
         drop(item, monitor) {
+
             let draggedItemId = item.id;
             let dropBlockIndex = props.blockIndex;
+            console.log(draggedItemId, dropBlockIndex)
             moveItemToBlock(draggedItemId, dropBlockIndex);
         }
     });
@@ -80,8 +111,10 @@ const BlockItem = (props) => {
             </IconButton>
 
             {props.blockBody.map((component, index) => {
-                    return <ComponentItem {...component} index={index} blockIndex={props.blockIndex}
-                                          onSelectItem={onSelectComponent} moveCard={moveItem}/>
+                    return <ComponentItem children={children} setChildren={setChildren} {...component} index={index}
+                                          blockIndex={props.blockIndex}
+                                          onSelectItem={onSelectComponent} moveCard={moveItem}
+                                          moveItemIntoDiv={moveItemIntoDiv}/>
                 }
             )}
         </BlockWrapper>
--- src/Components/BodyContainer/Canvas/ComponentItem/ComponentItem.jsx
+++ src/Components/BodyContainer/Canvas/ComponentItem/ComponentItem.jsx
@@ -1,10 +1,21 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import {useDrag, useDrop} from "react-dnd";
 import {ItemTypes} from "../../../../ItemTypesDND";
 
 
-
-const ComponentItem = ({id, index, blockIndex, componentType, css, html, onSelectItem, moveCard}) => {
+let children = [];
+const ComponentItem = ({
+                           id,
+                           index,
+                           blockIndex,
+                           parentId,
+                           componentType,
+                           css,
+                           html,
+                           onSelectItem,
+                           moveCard,
+                           moveItemIntoDiv
+                       }) => {
     const ref = useRef(null);
 
     const [{handlerId}, drop] = useDrop({
@@ -27,15 +38,56 @@ const ComponentItem = ({id, index, blockIndex, componentType, css, html, onSelec
             }
             const hoverBoundingRect = ref.current?.getBoundingClientRect();
             const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+            const hoverMiddleX = (hoverBoundingRect.left - hoverBoundingRect.right) / 2;
+
+
             const clientOffset = monitor.getClientOffset();
             const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+            const hoverClientX = clientOffset.x - hoverBoundingRect.right;
+
+            const inInnerHoverY = hoverClientY < hoverMiddleY + hoverBoundingRect.height / 4 && hoverClientY > hoverMiddleY - hoverBoundingRect.height / 4;
+            const inInnerHoverX = hoverClientX < hoverMiddleX + hoverBoundingRect.height / 4 && hoverClientX > hoverMiddleX - hoverBoundingRect.height / 4;
+            if (inInnerHoverY && inInnerHoverX) {
+                return;
+            }
+
             if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
                 return;
             }
+
             if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
                 return;
             }
+
             moveCard(dragIndex, dragId, hoverIndex);
+            item.index = hoverIndex;
+        },
+
+        drop(item, monitor) {
+            if (!ref.current) {
+                return;
+            }
+            const dragIndex = item.index;
+            const dragId = item.id;
+            const hoverIndex = index;
+            if (dragIndex === hoverIndex) {
+                return;
+            }
+            const hoverBoundingRect = ref.current?.getBoundingClientRect();
+            const divMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+            const divMiddleX = (hoverBoundingRect.left - hoverBoundingRect.right) / 2;
+
+
+            const clientOffset = monitor.getClientOffset();
+            const ClientY = clientOffset.y - hoverBoundingRect.top;
+            const ClientX = clientOffset.x - hoverBoundingRect.right;
+
+            const inInnerY = ClientY < divMiddleY + hoverBoundingRect.height / 4 && ClientY > divMiddleY - hoverBoundingRect.height / 4;
+            const inInnerX = ClientX < divMiddleX + hoverBoundingRect.height / 4 && ClientX > divMiddleX - hoverBoundingRect.height / 4;
+            if (inInnerY && inInnerX) {
+                moveItemIntoDiv(dragIndex, dragId, hoverIndex);
+                return;
+            }
             item.index = hoverIndex;
         }
     });
@@ -50,11 +102,10 @@ const ComponentItem = ({id, index, blockIndex, componentType, css, html, onSelec
     });
     drag(drop(ref));
 
-    if (componentType==="div"){
 
-    }
-    return (
-        React.createElement(`${componentType}`,
+    let result;
+    if (parentId !== 0) {
+        children.push(React.createElement(`${componentType}`,
             {
                 onClick: () => {
                     onSelectItem(id, blockIndex)
@@ -62,7 +113,36 @@ const ComponentItem = ({id, index, blockIndex, componentType, css, html, onSelec
                 ref: ref,
                 style: css,
                 ...html
-            }, (html?.content)))
+            }, (html?.content)));
+        result = <></>;
+    } else {
+        if (componentType === "img")
+            result = React.createElement(`${componentType}`,
+                {
+                    onClick: () => {
+                        onSelectItem(id, blockIndex)
+                    },
+                    ref: ref,
+                    style: css,
+                    ...html
+                }, (html?.content))
+        else {
+            result = React.createElement(`${componentType}`,
+                {
+                    onClick: () => {
+                        onSelectItem(id, blockIndex)
+                    },
+                    ref: ref,
+                    style: css,
+                    ...html
+                }, (html?.content), [children])
+            children = []
+        }
+
+    }
+
+
+    return result;
 }
 
 export default ComponentItem;
